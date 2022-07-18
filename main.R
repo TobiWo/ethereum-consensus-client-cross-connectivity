dc905c596db211e0bc379d630dddf3d2ac14a17a
TobiWo <Emailkeeper@gmx.de>
2022-07-18 10:35:26 +0200

    feat(scripts): Improve main script for better contributor experience

--- main.R
+++ main.R
@@ -12,8 +12,16 @@ lapply(
   }
 )
 
-setwd(dirname(getSourceEditorContext()$path))
+if (Sys.getenv("RSTUDIO") == "1") {
+  setwd(dirname(getSourceEditorContext()$path))
+}
 
-source("./scripts/load_data.R")
-source("./scripts/create_data_source_for_plot.R")
-source("./scripts/create_plot.R")
+try_result <- try(source("./scripts/load_data.R"))
+if (class(try_result) == "try-error") {
+  print("You did not set the working directory (with 'setwd()') to the root folder of the repository!")
+  print("Please do so and execute again!")
+} else {
+  source("./scripts/create_data_source_for_plot.R")
+  source("./scripts/create_plot.R")
+}
+
