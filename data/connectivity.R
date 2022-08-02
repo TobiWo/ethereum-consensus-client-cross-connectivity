84a099f0fddaaee9691fae6a4492f6e59dee5a3a
TobiWo <Emailkeeper@gmx.de>
2022-08-02 16:30:49 +0200

    feat(plot): Update connectivity data and plot

--- data/connectivity.R
+++ data/connectivity.R
@@ -4,7 +4,7 @@
 # NA    == not checked yet
 
 # CL clients in format <client name>_<release or tag>
-clients <- c("lighthouse_v2.3.1", "teku_develop", "nimbus_v22.6.1", "prysm", "lodestar_v0.38.1")
+clients <- c("lighthouse_v2.5.0", "teku_v22.7.0", "nimbus_v22.7.0", "prysm", "lodestar_v0.41.0")
 
 client_combinations <- c(
   "lighthouse_lighthouse",
@@ -37,23 +37,23 @@ client_combinations <- c(
 # lighthouse beacon client with validator client x
 lighthouse_lighthouse <- TRUE
 lighthouse_teku <- TRUE
-lighthouse_nimbus <- FALSE
+lighthouse_nimbus <- TRUE
 lighthouse_prysm <- NA
-lighthouse_lodestar <- NA
+lighthouse_lodestar <- TRUE
 
 # teku beacon client with validator client x
 teku_lighthouse <- TRUE
 teku_teku <- TRUE
-teku_nimbus <- FALSE
+teku_nimbus <- TRUE
 teku_prysm <- NA
-teku_lodestar <- NA
+teku_lodestar <- TRUE
 
 # nimbus beacon client with validator client x
 nimbus_lighthouse <- TRUE
 nimbus_teku <- TRUE
 nimbus_nimbus <- TRUE
 nimbus_prysm <- NA
-nimbus_lodestar <- NA
+nimbus_lodestar <- FALSE
 
 # prysm beacon client with validator client x
 prysm_lighthouse <- NA
@@ -63,8 +63,8 @@ prysm_prysm <- TRUE
 prysm_lodestar <- NA
 
 # lodestar beacon client with validator client x
-lodestar_lighthouse <- NA
-lodestar_teku <- NA
-lodestar_nimbus <- NA
+lodestar_lighthouse <- TRUE
+lodestar_teku <- FALSE
+lodestar_nimbus <- TRUE
 lodestar_prysm <- NA
 lodestar_lodestar <- TRUE
