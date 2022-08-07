88871589f003bb0fa0f09a025b876295da093619
TobiWo <Emailkeeper@gmx.de>
2022-08-07 10:00:30 +0200

    feat(code): Improve utils code
    * Improve code so that vc can be added without a bn row
    * Add logic which considers the newly introduced 'warning' label

--- scripts/utils.R
+++ scripts/utils.R
@@ -1,34 +1,38 @@
 create_beacon_client_vectors <- function(client_combinations, clients) {
-  for (client_combination in client_combinations) {
-    last_beacon <- strsplit(
-      x = client_combination,
-      split = "_", fixed = TRUE
-    )[[1]][1]
-    if (!exists("current_beacon") || last_beacon != current_beacon) {
-      current_beacon <- last_beacon
-    }
-    if (!exists(current_beacon)) {
-      assign(current_beacon, get(client_combination))
-    } else {
-      assign(current_beacon, c(get(current_beacon), get(client_combination)))
-    }
-  }
   client_vectors <- list()
-  for (client in clients) {
-    client <- strsplit(x = client, split = "_", fixed = TRUE)[[1]][1]
-    client_vectors <- append(client_vectors, list(get(client)))
-    names(client_vectors)[length(client_vectors)] <- client
+  current_beacon_name <- strsplit(x = client_combinations[1], split = "_", fixed = TRUE)[[1]][1]
+  client_vector_to_add <- NA
+  current_client_vector <- c()
+  for (client_combination in client_combinations) {
+    last_beacon_name <- strsplit(x = client_combination, split = "_", fixed = TRUE)[[1]][1]
+    if (
+      last_beacon_name != current_beacon_name ||
+      client_combination == client_combinations[length(client_combinations)]
+    ) {
+      client_vector_to_add <- current_beacon_name
+      current_beacon_name <- last_beacon_name
+      if (client_combination == client_combinations[length(client_combinations)]) {
+        current_client_vector <- c(current_client_vector, get(client_combination))
+      }
+      client_vectors <- append(client_vectors, list(current_client_vector))
+      names(client_vectors)[length(client_vectors)] <- client_vector_to_add
+      current_client_vector <- c()
+    }
+    current_client_vector <- c(current_client_vector, get(client_combination))
   }
   return(client_vectors)
 }
-
 
 create_plot_data_source <- function(client_vectors) {
   client_matrix_expression <- "tibble(clients"
   counter <- 1
   for (item in client_vectors) {
     client <- names(client_vectors)[counter]
+    warning_indices <- which(item == WARNING)
+    item <- replace(item, warning_indices, TRUE)
+    item <- as.logical(item)
     item <- as.integer(item)
+    item <- replace(item, warning_indices, 0.75)
     na_indices <- which(is.na(item))
     item <- replace(item, na_indices, 0.5)
     assign(client, item)
